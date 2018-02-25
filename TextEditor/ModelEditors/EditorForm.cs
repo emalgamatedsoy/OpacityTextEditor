3098bd66612d278327aacbcb08dc9d5729f92f8d
unknown <mcsherry.patrick@gmail.com>
2018-02-24 19:26:13 -0500

    Forgot about this. Changed the cs file folders around, and changed the static variables for the settings.cs

---
+++ TextEditor/ModelEditors/EditorForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TextEditor
+{
+    class EditorForm
+    {
+        private RichTextBox rtb;
+        List<Button> buttons = new List<Button>();
+        int buttonCount;
+
+        EditorForm(RichTextBox rtb, int buttonCount)
+        {
+            this.rtb = rtb;
+            createButtons(buttonCount);
+            this.buttonCount = buttonCount;
+        }
+
+        private void createButtons(int buttonCount) 
+        {
+            for (int i = 0; i < buttonCount; i++)
+            {
+                buttons.Add(new Button());
+            }
+        }
+
+        public Button GetButton(int i)
+        {
+            if (buttons.ElementAt(i) == null)
+                throw new NullReferenceException("Button not found");
+            else
+                return buttons.ElementAt(i);
+        }
+
+        public RichTextBox GetRichTextBox()
+        {
+            return rtb;
+        }
+    }
+}
