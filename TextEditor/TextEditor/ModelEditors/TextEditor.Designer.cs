f2d5ed47f6393aca2d624b3ea25d9d962bfe37e3
McSherry <pm0398@ccsu.edu>
2018-04-16 17:00:20 -0400

    changed around the file folders, and blew away a cs file I wasn't using.

---
+++ TextEditor/TextEditor/ModelEditors/TextEditor.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TextEditor.Main
+{
+    partial class TextEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TextEditor));
+            this.RichTextEditor = new System.Windows.Forms.RichTextBox();
+            this.LoadBtn = new System.Windows.Forms.Button();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.SettingsBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // RichTextEditor
+            // 
+            this.RichTextEditor.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RichTextEditor.Location = new System.Drawing.Point(13, 13);
+            this.RichTextEditor.Name = "RichTextEditor";
+            this.RichTextEditor.Size = new System.Drawing.Size(502, 313);
+            this.RichTextEditor.TabIndex = 0;
+            this.RichTextEditor.Text = "";
+            // 
+            // LoadBtn
+            // 
+            this.LoadBtn.Location = new System.Drawing.Point(12, 332);
+            this.LoadBtn.Name = "LoadBtn";
+            this.LoadBtn.Size = new System.Drawing.Size(45, 23);
+            this.LoadBtn.TabIndex = 1;
+            this.LoadBtn.Text = "Load";
+            this.LoadBtn.UseVisualStyleBackColor = true;
+            this.LoadBtn.Click += new System.EventHandler(this.LoadBtn_Click);
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(63, 332);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(45, 23);
+            this.SaveBtn.TabIndex = 2;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // SettingsBtn
+            // 
+            this.SettingsBtn.Location = new System.Drawing.Point(458, 332);
+            this.SettingsBtn.Name = "SettingsBtn";
+            this.SettingsBtn.Size = new System.Drawing.Size(57, 23);
+            this.SettingsBtn.TabIndex = 3;
+            this.SettingsBtn.Text = "Settings";
+            this.SettingsBtn.UseVisualStyleBackColor = true;
+            this.SettingsBtn.Click += new System.EventHandler(this.SettingsBtn_Click);
+            // 
+            // TextEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(527, 367);
+            this.Controls.Add(this.SettingsBtn);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.LoadBtn);
+            this.Controls.Add(this.RichTextEditor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "TextEditor";
+            this.Text = "Text Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextEditor_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox RichTextEditor;
+        private System.Windows.Forms.Button LoadBtn;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.Button SettingsBtn;
+    }
+}
+
---
+++ TextEditor/TextEditor/ModelEditors/TextEditor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TextEditor.LoadSave;
+using TextEditor.Settings;
+using TextEditor.ModelEditors;
+
+namespace TextEditor.Main
+{
+    public partial class TextEditor : Form
+    {
+        private Load load;
+        private Save save;
+        public TextEditor()
+        {
+            InitializeComponent();
+            CreateEditorForm();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            load = new Load();
+            save = new Save();
+        }
+
+        private void CreateEditorForm()
+        {
+            
+        }
+
+        private void UpdateEditorForm()
+        {
+        }
+
+        private void SaveTextDoc()
+        {
+            TextEditorModObj.text = RichTextEditor.Text;
+            try
+            {
+                save.SaveTextFile(TextEditorModObj.text);
+            }
+            catch (ArgumentNullException) { }
+
+        }
+
+        private void LoadTextDoc()
+        {
+            load.LoadTextFile();
+            if (load.GetLoadState())
+                TextEditorModObj.text = load.GetText();
+
+            RichTextEditor.Text = TextEditorModObj.text;
+        }
+
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            SaveTextDoc();
+        }
+
+        private void LoadBtn_Click(object sender, EventArgs e)
+        {
+            LoadTextDoc();
+        }
+
+        private void SettingsBtn_Click(object sender, EventArgs e)
+        {
+            SettingsEditor settings = new SettingsEditor();
+            settings.ShowDialog();
+            this.Opacity = settings.GetSettingOpacity();
+
+        }
+        
+        private void TextEditor_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.O && e.Modifiers == Keys.Control)
+                LoadTextDoc();
+            else if (e.KeyCode == Keys.S && e.Modifiers == Keys.Control)
+                SaveTextDoc();
+        }
+
+    }
+}
